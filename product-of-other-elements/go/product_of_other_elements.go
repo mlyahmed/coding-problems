f7b4e4a7827800398a567218dd2a102b71746bdc
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2020-11-25 09:34:11 +0100

    Product Of Other Elements - V2

--- product-of-other-elements/go/product_of_other_elements.go
+++ product-of-other-elements/go/product_of_other_elements.go
@@ -2,15 +2,24 @@ package productofotherlements
 
 // ProductOf returns a new array such that each element at index i of the new array
 // is the product of all the numbers in the original array except the one at i.
+// Thanks to https://leetcode.com/problems/product-of-array-except-self/solution/.
+// Time Comlexity : O(n).
+// Space Complexity : O(n).
+// where n=len(list).
 func ProductOf(list []int) []int {
-	total := 1
 	result := make([]int, len(list))
-	for _, v := range list {
-		total = total * v
+
+	for i := range list {
+		result[i] = 1
 	}
 
-	for i, v := range list {
-		result[i] = total / v
+	for i := 1; i < len(list); i++ {
+		result[i] = result[i-1] * list[i-1]
+	}
+
+	for r, i := 1, len(list)-2; i >= 0; i-- {
+		r = r * list[i+1]
+		result[i] *= r
 	}
 
 	return result
